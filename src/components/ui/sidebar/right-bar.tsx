043f5e676304760b8f3b46951e94adc01a573f9b
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2025-03-04 15:54:19 -0600

    Update right-bar.tsx

--- src/components/ui/sidebar/right-bar.tsx
+++ src/components/ui/sidebar/right-bar.tsx
@@ -44,7 +44,7 @@ export function RightSidebar() {
           <Search className="absolute left-4 top-3 h-5 w-5 text-gray-400" />
           <input
             type="text"
-            placeholder="Search Nexus"
+            placeholder="Search"
             className="w-full rounded-full bg-gray-100 py-2 pl-12 pr-4 text-sm text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-tertiary dark:bg-primary dark:text-white dark:placeholder-gray-500 transition-all"
           />
         </div>
