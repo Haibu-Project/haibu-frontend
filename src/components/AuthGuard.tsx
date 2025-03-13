ee6ca9bb296ff9dc233271bf1d74d3d7d2545025
villarley <santivillarley1010@gmail.com>
2025-03-13 17:38:45 -0300

    hotfix: register and login working

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useAddress } from "@chopinframework/react";
 import { ReactNode } from "react";
+import { useAuthCheck } from "@/hooks/useAuthChek";
 
 export default function AuthGuard({ children }: { children: ReactNode }) {
   const { address, isLoading } = useAddress();
@@ -12,6 +13,7 @@ export default function AuthGuard({ children }: { children: ReactNode }) {
     router.push("/auth/register");
   }
 
+  useAuthCheck();
 
   return <>{children}</>;
 }
